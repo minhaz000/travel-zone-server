93417a43164556fcd38aa486c49c4770d9dc763f
minhaz000 <mrmminhaz@gamail.com>
2023-03-16 12:16:06 +0600

    fliter are done with ducumentaton

--- connectDB.js
+++ connectDB.js
@@ -1,7 +1,7 @@
 const {MongoClient } = require('mongodb')
 require('dotenv').config()
 // DataBase Connection
-const  url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ah6p2gy.mongodb.net/xstream`
+const  url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ah6p2gy.mongodb.net/`
  const client = new MongoClient(url)
 exports.connectDB =  async ()=>{
       try {
